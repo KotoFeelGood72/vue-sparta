2fb7140ddd69640e848e2e9011bda25eab30181f
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-11-03 09:56:24 +0300

    Сверстал блок о нас

---
+++ src/components/blocks/AboutBlock.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="about-block">
+    <div class="container">
+      <div>
+        <div>
+          <img src="" alt="">
+        </div>
+        <div>
+          <h3>О компании</h3>
+          <div>
+            <p>Gd ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam</p>
+            <p>Kst, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatu</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <!-- Иконка -->
+             <div>
+                <h5>Sed ut perspiciatis </h5>
+                <p>ed ut perspiciatis unde omnis i natus error sit voluptatem accusantium doloremque laudantium, totam rem </p>
+             </div>
+          </li>
+          <li>
+            <!-- Иконка -->
+             <div>
+                <h5>Sed ut perspiciatis </h5>
+                <p>ed ut perspiciatis unde omnis i natus error sit voluptatem accusantium doloremque laudantium, totam rem </p>
+             </div>
+          </li>
+          <li>
+            <!-- Иконка -->
+             <div>
+                <h5>Sed ut perspiciatis </h5>
+                <p>ed ut perspiciatis unde omnis i natus error sit voluptatem accusantium doloremque laudantium, totam rem </p>
+             </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
