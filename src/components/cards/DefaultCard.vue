9de815fbbc1c210eb5b8e8ce475724c05585d293
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-11-03 09:41:27 +0300

    Сделал карточки обычную

---
+++ src/components/cards/DefaultCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+
+export interface DefaultCardModel {
+  image: string;
+  title: string;
+  id: string
+  slug: string
+}
+
+
+const { image, title, id, slug } = defineProps<DefaultCardModel>();
+
+</script>
+
+<template>
+  <RouterLink :to="`/${slug}/${id}`">
+    <div>
+      <img :src="image" :alt="title">
+    </div>
+    <div>
+      <h3>{{ title }}</h3>
+      <button type="button">Заказать</button>
+    </div>
+  </RouterLink>
+</template>
