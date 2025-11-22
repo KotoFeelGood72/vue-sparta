2e6b9b96f58161a58cad055fc8588a89afdca570
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-11-22 15:28:48 +0300

    Собрал детальную страницу услуги

--- src/pages/repair-page/RepairDetailPage.vue
+++ src/pages/repair-page/RepairDetailPage.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
+import BlockForm from '@/components/shared/BlockForm.vue';
 
 </script>
 
 <template>
-  <div>
-
+  <div class="repair-page pt-6">
+    <div class="container">
+      <h1>Ремонт ДВС и КПП </h1>
+      <div>
+        <div>
+          <img src="" alt="">
+        </div>
+        <div>
+          <p>Gd ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+          </p>
+          <p>
+    Kst, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatu</p>
+        </div>
+      </div>
+    </div>
+    <BlockForm
+      image="/images/form-one.png"
+      title="закажите экспресс-доставку"
+      subtitle="Если у Вас есть сложности с выбором товара или другие вопросы, то Вы можете получить консультацию у наших менеджеров
+в рабочие часы компании."
+      theme="white"
+    />
   </div>
 </template>
+
+<style scoped>
+
+</style>
+
